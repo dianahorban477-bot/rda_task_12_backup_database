257de6631da86ee2db4036d63625ee8bc8210290
dianahorban <dianahorban477@gmail.com>
2026-05-22 18:50:57 +0200

    The solution

--- task.sh
+++ task.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 DB_USER='backup'
 DB_PASSWORD='P@ssw0rd'
-mysqldumbp -u'backup' -p'P@ssw0rd' ShopDB > backup_full.sql
-mysql -u'backup' -p'P@ssw0rd' ShopDBReserve < backup_full.sql
-mysqldumbp -u'backup' -p'P@ssw0rd' ShopDB --no-create-db --no-create-info ShopDB > data_only_backup.sql
-mysql -u'backup' -p'P@ssw0rd' ShopDBDevelopment < data_only_backup.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db ShopDB > backup_full.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < backup_full.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db --no-create-info ShopDB > data_only_backup.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < data_only_backup.sql
